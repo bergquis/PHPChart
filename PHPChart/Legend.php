2265a4626c500f3e9eb60290201f0490c757543c
Ole Markus With <o.with@betradar.com>
2009-08-12 15:55:04 +0200

    CHANGE: Fixed 'thickness' issue with fonts in the legend

--- PHPChart/Legend.php
+++ PHPChart/Legend.php
@@ -34,25 +34,25 @@ class PHPChart_Legend extends PHPChart_Component {
 		
 		foreach($this->labels as $label => $color) {
 			$this->chart->getDriver()->setStrokeColor($color);
-			$this->chart->getDriver()->setFillColor($color);
+			$this->chart->getDriver()->setFillColor("#00ff0000");
 			
 			$dimensions = $this->chart->getDriver()->getTextDimensions($label);
-			
+			$this->chart->getDriver()->setFillColor("$color");
 			if ($this->position & PHPChart::BOTTOM) {
 				$by = $this->ybottom - ($this->height - $dimensions[1]) / 2;
 				$this->chart->getDriver()->drawRectangle($bx, $by,  $bx + $dimensions[1], $by - $dimensions[1]);
-				$this->chart->getDriver()->setStrokeColor('transparent');
-				$this->chart->getDriver()->setStrokeColor($this->strokeColor);
+				$this->chart->getDriver()->setStrokeColor('#00ff0000');
+				$this->chart->getDriver()->setFillColor($this->strokeColor);
 				$this->chart->getDriver()->text($bx + $dimensions[2] * 1.5, $by -1, $label);
 				$bx += $dimensions[0] + $dimensions[1] + $dimensions[2] * 2;
 			}
 			if ($this->position & PHPChart::RIGHT) {
 				
 				$this->chart->getDriver()->drawRectangle($bx, $by,  $bx + $dimensions[1], $by + $dimensions[1]-2);
-				$this->chart->getDriver()->setStrokeColor('transparent');
+				$this->chart->getDriver()->setStrokeColor('#00ff0000');
 				$this->chart->getDriver()->setFillColor($this->strokeColor);
 				$this->chart->getDriver()->text($bx + $dimensions[1] + $dimensions[2], $by + $dimensions[1]-2, $label);
-				$by += $dimensions[1] + $dimensions[1];
+				$by += $dimensions[1] * 1.5;
 			}  
 		}
 		
